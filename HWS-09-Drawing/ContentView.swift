a7386616838e46defdc8455b7ef5636152d7454a
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-02 17:44:21 +0200

    Initial Commit

---
+++ HWS-09-Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWS-09-Drawing
+//
+//  Created by Manuel Casocavallo on 02/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS-09-Drawing/HWS_09_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_09_DrawingApp.swift
+//  HWS-09-Drawing
+//
+//  Created by Manuel Casocavallo on 02/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_09_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
